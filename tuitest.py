828509eeb87b4746a5837e33df24ebf231c54dd5
nguyendag <dvknguyen999@gmail.com>
2021-12-14 20:16:40 +0700

    rm test file

--- tuitest.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-
-pad_ms = 1000  # milliseconds of silence needed
-silence = AudioSegment.silent(duration=pad_ms)
-audio = AudioSegment.from_wav('thu4.wav')
-
-padded = audio + silence  # Adding silence after the audio
-padded.export('padded-file.wav', format='wav')
